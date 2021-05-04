3cc3713995a693f7bfd8dd30ecb6712cc2704866
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-05-04 14:03:54 +0700

    Pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,31 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// echo, print untuk menampilkan ke layar
+// print_r untuk mencetak array
+// var_dump untuk mencetak variabel
+
+// Penulisan syntaks PHP
+// 1.php dalam html
+// 2.html didalam php
+
+// Variabel dan Tipe Data
+// Variabel
+$nama = "Harun";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Hallo Selamat Datang <?php echo $nama ;?></h1>
+
+    <?php echo"<h1>Hallo Selamat Datang</h1>"; ?>
+</body>
+</html>
