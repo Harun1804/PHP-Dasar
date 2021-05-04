0361fb56cce0f66a914fbe24555b7106573e2e2b
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-05-04 17:45:35 +0700

    function

---
+++ pertemuan4/date.php
@@ -0,0 +1,12 @@
+<?php
+// Build in Function
+// echo date("l");
+// echo time();
+// echo date("d M Y", time() - 60 * 60 * 24 * 100);
+// mktime membuat detik sendiri
+// jam, menit , detik, bulan,tgl ,taun
+// echo date("l",mktime(0,0,0,06,07,2000));
+
+// kebalikan dari mktime
+// echo strtotime("06 07 2000");
+?>
